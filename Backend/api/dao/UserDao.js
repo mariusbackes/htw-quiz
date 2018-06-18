b08399a36726b9fd0ff198efeb0be4f12aa3bf95
mariusbackes <mariusbackes@icloud.com>
2018-06-18 19:53:46 +0200

    UserTest + Methoden für Benutzerverwaltung

--- Backend/api/dao/UserDao.js
+++ Backend/api/dao/UserDao.js
@@ -16,3 +16,57 @@ exports.register = (user_data) => {
         });
     });
 }
+
+exports.login = (user_data) => {
+    let return_value = { success: false };
+
+    // TODO: Update last_login
+    let sql = "";
+
+    return new Promise((resolve, reject) => {
+        mysql.query(sql, [user_data], (error, result) => {
+            if (error) throw error;
+            return_value.success = result.affectedRows > 0;
+            resolve(return_value);
+        });
+    });
+}
+
+exports.change_password = (data) => {
+    let return_value = { success: false };
+    let sql = "update user set password = :password where user_id = :user_id";
+
+    return new Promise((resolve, reject) => {
+        mysql.query(sql, {password: data.password, user_id: user_id}, (error, result) => {
+            if (error) throw error;
+            return_value.success = result.affectedRows > 0;
+            resolve(return_value);
+        });
+    });
+}
+
+exports.change_email = (data) => {
+    let return_value = { success: false };
+    let sql = "update user set email = :email where user_id = :user_id";
+
+    return new Promise((resolve, reject) => {
+        mysql.query(sql, {email: data.email, user_id: user_id}, (error, result) => {
+            if (error) throw error;
+            return_value.success = result.affectedRows > 0;
+            resolve(return_value);
+        });
+    });
+}
+
+exports.change_name = (data) => {
+    let return_value = { success: false };
+    let sql = "update user set first_name = :first_name, last_name = :last_name where user_id = :user_id";
+
+    return new Promise((resolve, reject) => {
+        mysql.query(sql, {first_name: data.first_name, last_name: data.last_name, user_id: user_id}, (error, result) => {
+            if (error) throw error;
+            return_value.success = result.affectedRows > 0;
+            resolve(return_value);
+        });
+    });
+}
--- test/UserTest.js
+++ test/UserTest.js
@@ -5,35 +5,69 @@
 import { expect } from 'chai';
 import request from 'request-promise';
 
-const url = 'http://localhost:8080';
+const url = 'http://localhost:8001';
 const options = {
   method: 'POST'
 };
 
 describe("User API-Methods", () => {
-    it("register", async () => {
+    it("register success", async () => {
         options.form = {
-
+          username: "mariusb73",
+          email: "mariusbackes@icloud.com",
+          first_name: "Marius",
+          last_name: "Backes",
+          password: "test123"
         };
         let data = await doRequest(options, "/user/register");
         expect(data.success).to.equal(true);
         expect(data.userdata).not.empty;
     });
 
-    it("login", async () => {
-
+    it("login success", async () => {
+      options.form = {
+        username: "mariusb73",
+        email: "mariusbackes@icloud.com",
+        password: "test123"
+      };
+      let data = await doRequest(options, "/user/login");
+      expect(data.success).to.equal(true);
+      expect(data.userdata).not.empty;
     });
 
-    it("change_password", async () => {
-
+    it("change_password success", async () => {
+      options.form = {
+        user_id: "1",
+        username: "mariusb73",
+        email: "mariusbackes@icloud.com",
+        password: "test123"
+      };
+      let data = await doRequest(options, "/user/change_password");
+      expect(data.success).to.equal(true);
     });
 
-    it("change_email", async () => {
-
+    it("change_email success", async () => {
+      options.form = {
+        user_id: "1",
+        username: "mariusb73",
+        email: "mariusbackes@icloud.com",
+        password: "test123"
+      };
+      let data = await doRequest(options, "/user/change_email");
+      expect(data.success).to.equal(true);
     });
 
-    it("change_name", async () => {
-
+    it("change_name success", async () => {
+      options.form = {
+        user_id: "1",
+        username: "mariusb73",
+        email: "mariusbackes@icloud.com",
+        password: "test123",
+        first_name: "Marius",
+        last_name: "Backes"
+      };
+      let data = await doRequest(options, "/user/change_name");
+      expect(data.success).to.equal(true);
     });
 });
 
