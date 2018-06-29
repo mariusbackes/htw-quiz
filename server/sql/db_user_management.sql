7b9a4e524916a4d6e60abb06b30c09c6733c33fe
Marius Backes <mariusbackes@icloud.com>
2018-06-29 17:16:13 +0200

    created database models

--- server/sql/db_user_management.sql
+++ server/sql/db_user_management.sql
@@ -1,13 +1,13 @@
 # Datenbank-Nutzer anlegen
 
 create user if not exists 'htw_quiz_user'@'%' identified by 'X8MpBzYzwK';
-grant select, insert, update, delete on htw-quiz.user to 'htw_quiz_user'@'%';
-grant select, insert, update on htw-quiz.highscore to 'htw_quiz_user'@'%';
-grant select, insert, update, delete on htw-quiz.question to 'htw_quiz_user'@'%';
-grant select, insert, update, delete on htw-quiz.multiple_choice to 'htw_quiz_user'@'%';
-grant select, insert, update, delete on htw-quiz.game to 'htw_quiz_user'@'%';
-grant select, insert, update, delete on htw-quiz.contributors to 'htw_quiz_user'@'%';
-grant select, insert, update on htw-quiz.time_frame to 'htw_quiz_user'@'%';
-grant execute on htw-quiz.* to 'htw_quiz_user'@'%';
+grant select, insert, update, delete on htw_quiz.user to 'htw_quiz_user'@'%';
+grant select, insert, update on htw_quiz.highscore to 'htw_quiz_user'@'%';
+grant select, insert, update, delete on htw_quiz.question to 'htw_quiz_user'@'%';
+grant select, insert, update, delete on htw_quiz.multiple_choice to 'htw_quiz_user'@'%';
+grant select, insert, update, delete on htw_quiz.game to 'htw_quiz_user'@'%';
+grant select, insert, update, delete on htw_quiz.contributors to 'htw_quiz_user'@'%';
+grant select, insert, update on htw_quiz.time_frame to 'htw_quiz_user'@'%';
+grant execute on htw_quiz.* to 'htw_quiz_user'@'%';
 
 flush privileges;
