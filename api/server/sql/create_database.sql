27284bdff4d0712011ce38e7b6ba451c6f754d67
Marius Backes <mariusbackes@icloud.com>
2018-06-29 17:58:43 +0200

    create databse script

---
+++ api/server/sql/create_database.sql
@@ -0,0 +1,226 @@
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: htw_quiz
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+create database if not exists htw_quiz;
+use htw_quiz;
+
+--
+-- Table structure for table `contributors`
+--
+
+DROP TABLE IF EXISTS `contributors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contributors` (
+  `game_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `play` tinyint(4) NOT NULL,
+  `create` tinyint(4) NOT NULL,
+  `edit` tinyint(4) NOT NULL,
+  `delete` tinyint(4) NOT NULL,
+  KEY `contributors_user_user_id_fk` (`user_id`),
+  CONSTRAINT `contributors_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contributors`
+--
+
+LOCK TABLES `contributors` WRITE;
+/*!40000 ALTER TABLE `contributors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contributors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `game_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `creator` int(11) NOT NULL,
+  `challenged` tinyint(4) NOT NULL,
+  PRIMARY KEY (`game_id`),
+  KEY `game_user_user_id_fk` (`creator`),
+  CONSTRAINT `game_user_user_id_fk` FOREIGN KEY (`creator`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `highscore`
+--
+
+DROP TABLE IF EXISTS `highscore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `highscore` (
+  `game_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `score` int(11) DEFAULT NULL,
+  KEY `highscore_game_game_id_fk` (`game_id`),
+  KEY `highscore_user_user_id_fk` (`user_id`),
+  CONSTRAINT `highscore_game_game_id_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`),
+  CONSTRAINT `highscore_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `highscore`
+--
+
+LOCK TABLES `highscore` WRITE;
+/*!40000 ALTER TABLE `highscore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `highscore` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mutliple-choice`
+--
+
+DROP TABLE IF EXISTS `mutliple-choice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mutliple-choice` (
+  `question_id` int(11) NOT NULL,
+  `wrong_answer_1` varchar(350) NOT NULL,
+  `wrong_answer_2` varchar(350) DEFAULT NULL,
+  `wrong_answer_3` varchar(350) DEFAULT NULL,
+  KEY `mutliple-choice_questions_question_id_fk` (`question_id`),
+  CONSTRAINT `mutliple-choice_questions_question_id_fk` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mutliple-choice`
+--
+
+LOCK TABLES `mutliple-choice` WRITE;
+/*!40000 ALTER TABLE `mutliple-choice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `mutliple-choice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(350) NOT NULL,
+  `game_id` int(11) NOT NULL,
+  `correct_answer` varchar(250) NOT NULL,
+  `time_limit` int(11) DEFAULT NULL,
+  `is_multiple_choice` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`question_id`),
+  KEY `questions_game_game_id_fk` (`game_id`),
+  CONSTRAINT `questions_game_game_id_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `time-frame`
+--
+
+DROP TABLE IF EXISTS `time-frame`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `time-frame` (
+  `game_id` int(11) NOT NULL,
+  `from` datetime NOT NULL,
+  `to` datetime NOT NULL,
+  KEY `time-frame_game_game_id_fk` (`game_id`),
+  CONSTRAINT `time-frame_game_game_id_fk` FOREIGN KEY (`game_id`) REFERENCES `game` (`game_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `time-frame`
+--
+
+LOCK TABLES `time-frame` WRITE;
+/*!40000 ALTER TABLE `time-frame` DISABLE KEYS */;
+/*!40000 ALTER TABLE `time-frame` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `registered_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_games` int(11) NOT NULL DEFAULT '0',
+  `reached_points` int(11) NOT NULL DEFAULT '0',
+  `admin` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_email_uindex` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-06-29 17:53:01
