e6c43454d02c826bf50b8133eb357d30f626a6ac
Marius Backes <mariusbackes@icloud.com>
2018-07-27 19:46:15 +0200

    changeUsername + deleteUser in view

--- app/client/src/components/user_management/UserData.vue
+++ app/client/src/components/user_management/UserData.vue
@@ -8,45 +8,111 @@
               <v-expansion-panel-content>
                 <!-- E-Mail Adresse ändern -->
                 <div slot="header">E-Mail adresse ändern</div>
-                <div class="element-padding">
-                  <v-layout row wrap>
-                    <v-flex xs12 sm6 md3>
-                      <v-text-field p-2 outline label="Aktuelle E-Mail Adresse" :placeholder="user.email" disabled></v-text-field>
-                    </v-flex>
-                  </v-layout>
-                </div>
-
-                <div class="element-padding">
-                  <v-layout row wrap>
-                    <v-flex xs12 sm6 md3>
-                      <v-form v-model="changeEmailFormValid">
-                        <v-text-field p-2 outline label="Neue E-Mail Adresse" v-model="new_email" :rules="emailRules" required></v-text-field>
-                      </v-form>
-                    </v-flex>
-                  </v-layout>
-                </div>
-
                 <div class="text-xs-center">
-                  <v-btn color="primary" @click="changeEmail()" :disabled="!changeEmailFormValid">
-                    Änderung speichern
-                  </v-btn>
+                  <div class="element-padding">
+                    <v-layout row wrap>
+                      <v-flex >
+                        <v-text-field p-2 outline label="Aktuelle E-Mail Adresse" :placeholder="user.email" disabled></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </div>
+
+                  <div class="element-padding">
+                    <v-layout row wrap>
+                      <v-flex>
+                        <v-form v-model="changeEmailFormValid">
+                          <v-text-field p-2 outline label="Neue E-Mail Adresse" v-model="new_email" :rules="emailRules" required></v-text-field>
+                        </v-form>
+                      </v-flex>
+                    </v-layout>
+                  </div>
+
+                  <div>
+                    <v-btn color="primary" @click="changeEmail()" :disabled="!changeEmailFormValid">
+                      Änderung speichern
+                    </v-btn>
+                  </div>
                 </div>
               </v-expansion-panel-content>
 
               <!-- Username ändern -->
               <v-expansion-panel-content>
                 <div slot="header">Username ändern</div>
+                <div class="text-xs-center">
+                  <div class="element-padding">
+                    <v-layout row wrap>
+                      <v-flex >
+                        <v-text-field p-2 outline label="Aktueller Username" :placeholder="user.username" disabled></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </div>
+
+                  <div class="element-padding">
+                    <v-layout row wrap>
+                      <v-flex>
+                        <v-text-field p-2 outline label="Neuer Username" v-model="new_username" required ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </div>
+
+                  <div>
+                    <v-btn color="primary" @click="changeUsername()" :disabled="new_username.length == 0">
+                      Änderung speichern
+                    </v-btn>
+                  </div>
+                </div>
               </v-expansion-panel-content>
 
               <!-- Benutzer löschen -->
               <v-expansion-panel-content>
                 <div slot="header">Benutzer löschen</div>
+                <v-flex xs12 sm6 offset-sm3>
+                  <v-card>
+                    <v-card-title class="headline">Benutzer löschen</v-card-title>
+                    <v-card-text>Nach Bestätigung wird der Nutzer dauerhaft gelöscht! Alle erstellten Fragekataloge und Freigaben für andere Nutzer werden ebenfalls entfernt</v-card-text>
+                    <v-card-actions>
+                      <v-btn flat color="error" @click="showDeleteUserDialog = true">Benutzer löschen</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-flex>
               </v-expansion-panel-content>
             </v-expansion-panel>
           </v-flex>
         </v-layout>
       </v-card-text>
     </v-card>
+
+    <v-dialog
+      v-model="showDeleteUserDialog"
+      max-width="290">
+      <v-card>
+        <v-card-title class="headline">Wirklich löschen?</v-card-title>
+        <v-card-text>Bei Bestätigung wird der Nutzer gelöscht. Diese Aktion lässt sich nicht rückgängig machen</v-card-text>
+
+        <v-form ref="confirmPasswordForm" lazy-validation>
+          <v-text-field type="password" prepend-icon="security" label="Passwort eingeben" required
+                        v-model="confirm_password"
+                        :rules="confirm_passwordRules"></v-text-field>
+        </v-form>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            flat="flat"
+            @click="showDeleteUserDialog = false">
+            Abbrechen
+          </v-btn>
+
+          <v-btn
+            color="error"
+            flat="flat"
+            @click="deleteUser()">
+            Löschen
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -60,12 +126,22 @@
     data(){
       return {
         user: {},
+        // E-Mail managament
         new_email: "",
         changeEmailFormValid: false,
         emailRules: [
           v => !!v || 'E-Mail ist ein Pflichtfeld',
           v => /.+@.+/.test(v) || 'E-Mail muss gültig sein'
-        ]
+        ],
+        // Username management
+        new_username: "",
+        // Delete User management
+        showDeleteUserDialog: false,
+        confirm_password: "",
+        confirm_passwordRules: [
+          password => !!password || 'Passwort ist ein Pflichtfeld',
+          password => !!password && password.length >= 8 || 'Passwort muss mindestens 8 Zeichen enthalten'
+        ],
       }
     },
     methods: {
@@ -73,19 +149,35 @@
         this.user = globalService.getUser();
       },
       changeUsername() {
-        // TODO: Implement
+        userService.changeUsername(this.user, this.new_username).then((response) => {
+          if(response.success){
+            this.user.username = this.new_username;
+            this.storeUser();
+          }
+        })
       },
       changeEmail() {
         userService.changeEmail(this.user, this.new_email).then((response) => {
           if(response.success){
             this.user.email = this.new_email;
-            localStorage.setItem('user', JSON.stringify(this.user));
-            globalService.setUser(this.user);
+            this.storeUser();
           }
         })
       },
       deleteUser() {
-        // TODO: Implement
+        this.showDeleteUserDialog = false;
+        if (this.$refs.confirmPasswordForm.validate()) {
+          userService.deleteUser(this.user, this.confirm_password).then(response => {
+            if(response.success){
+              // TODO: "Logout"
+              // Localstorage löschen + auf die startseite navigieren
+            }
+          })
+        }
+      },
+      storeUser(){
+        localStorage.setItem('user', JSON.stringify(this.user));
+        globalService.setUser(this.user);
       }
     },
     mounted() {
--- app/client/src/components/user_management/UserInfo.vue
+++ app/client/src/components/user_management/UserInfo.vue
@@ -1,88 +1,88 @@
 <template>
-    <div>
-      <v-card flat>
-        <v-card-text>
-          <v-list two-line subheader>
-            <v-subheader inset>Benutzerinformationen</v-subheader>
+  <div>
+    <v-card flat>
+      <v-card-text>
+        <v-list two-line subheader>
+          <v-subheader inset>Benutzerinformationen</v-subheader>
 
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>face</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Benutzername</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.username}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>contacts</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Vorname</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.first_name}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>contacts</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Name</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.last_name}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>alternate_email</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>E-Mail Adresse</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.email}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>access_time</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Angemeldet seit</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.registered_at}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>face</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Benutzername</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.username}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>contacts</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Vorname</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.first_name}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>contacts</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Name</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.last_name}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>alternate_email</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>E-Mail Adresse</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.email}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>access_time</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Angemeldet seit</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.registered_at}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
 
-            <v-divider inset></v-divider>
-            <v-subheader inset>Spielinformationen</v-subheader>
+          <v-divider inset></v-divider>
+          <v-subheader inset>Spielinformationen</v-subheader>
 
-            <v-list-tile
-              avatar>
-              <v-list-tile-avatar>
-                <v-icon>games</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Absolvierte Spiele</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.completed_games}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
+          <v-list-tile
+            avatar>
+            <v-list-tile-avatar>
+              <v-icon>games</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Absolvierte Spiele</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.completed_games}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-avatar>
-                <v-icon>timeline</v-icon>
-              </v-list-tile-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Erreichte Punkte</v-list-tile-title>
-                <v-list-tile-sub-title>{{user.reached_points}}</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card-text>
-      </v-card>
-    </div>
+          <v-list-tile>
+            <v-list-tile-avatar>
+              <v-icon>timeline</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>Erreichte Punkte</v-list-tile-title>
+              <v-list-tile-sub-title>{{user.reached_points}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
