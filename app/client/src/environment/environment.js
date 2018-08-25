0949b8ecb2c03c3896de6f9fffdffdd8f390ccae
Tobias Meiser <meisertob@aol.com>
2018-08-25 13:46:17 +0200

    Präsentation an DB angepasst, Ausarbeitung  Konzeption

--- app/client/src/environment/environment.js
+++ app/client/src/environment/environment.js
@@ -1,4 +1,5 @@
 export const environment = {
+  //Die URLs müssen auf die Netzwerk-URL (sobald bekannt) umgestellt werden! 
   api_url: 'http://localhost:3000/api/',
   socket_url: 'http://localhost:3000/'
 };
